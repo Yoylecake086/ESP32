3bf66dd064b27b1afa0d6335ae057a83fc2b9616
DESKTOP-B1F00DK\Robocode 6 <robocodekh@gmail.com>
2025-04-05 16:52:08 +0200

    add func printDeviceAddress

--- bnuuyrabbuit/src/main.cpp
+++ bnuuyrabbuit/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include "BluetoothSerial.h"
+#include "esp_bt_device.h"
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+
+
+void printDeviceAddress() {
+  const uint8_t* address = esp_bt_dev_get_address();
+  for(int i = 0; i < 6; i++){
+    char str[3];
+    sprintf(str, "%02X", (int) address[i]);
+    Serial.print(str);
+    if(i < 5){
+      Serial.print(":");
+    }else{
+      Serial.print("\n");
+    }
+    }
+  }
+  /*
+  char addrStr[18];
+  sprintf(addrStr, "%02X:%02X:%02X:%02X:%02X:%02X",)
+  */
